d3bd9b3aa0576453918ad09024949060561aab39
Juan Pablo Amador Solis <pablo.amador@colorim.com.mx>
2021-06-29 15:04:33 -0500

    CredencialesVistaPDFCIMSA
    Creación de vista para PDF credencial CIMSA, Sin formato y datos

---
+++ resources/views/pdf/credencialCIMSA.blade.php
@@ -0,0 +1,211 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+
+    <title>Tarjetas</title>
+    <style type="text/css">
+        #razon1{ position:absolute;
+            z-index:1;
+            top:50px;
+            left:205px;
+            width:300px;
+            height:12px;
+            font-family: 'Roboto Slab', serif;
+            color: #FFFFFF;
+            font-size: 18px;
+        }
+
+        #nombres{ position:absolute;
+            z-index: 1;
+            top:225px;
+            left:90px;
+            width:auto;
+            height:auto;
+            font-size: 15px;
+            color: #FFFFFF;
+        }
+        #apellidos{ position:absolute;
+            z-index: 1;
+            top:240px;
+            left:86px;
+            width:auto;
+            height:auto;
+            font-size: 16px;
+            font-family: 'Roboto Slab', serif;
+            color: #FFFFFF;
+        }
+        #front{
+            position:absolute;
+            z-index:0;
+            width:502px;
+            height:325px;
+        }
+        #foto{ position:absolute;
+            z-index: 1;
+            top:75px;
+            left:75px;
+            width:295px;
+            height:354px;
+        }
+        #back{
+            position:absolute;
+            top: 25px;
+            left: 95px;
+            z-index:0;
+            width:502px;
+            height:325px;
+        }
+
+        #razon2{
+            position:absolute;
+            top: 50px;
+            left: 205px;
+            z-index:0;
+            width:auto;
+            height:auto;
+            font-family: 'Roboto';
+            font-size: 16px;
+            color: #008a57;
+        }
+        #calle{
+            position:absolute;
+            top: 80px;
+            left: 120px;
+            z-index:0;
+            width:auto;
+            height:auto;
+            font-family: 'Roboto';
+            font-size: 12px;
+            color: #e30043;
+        }
+    </style>
+</head>
+<body>
+<h1>Tarjetas</h1>
+<div class="container">
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">Nivel 1</div>
+            <div id="razon2"><span>Colorantes Importados SA de CV</span></div>
+            <div id="calle"><span>Calle 10 #20, Col. San Pedro de los Pinos,
+                <br>C.P. 01180, México, CDMX.
+                <br>Tel.  55 9149 5500          Lada sin costo: 80000 20000
+                <br>Número colaborador:
+                <br>NSS:
+                <br>Área:
+                <br>Vigencia: diciembre 2021
+                </span></div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">NIvel 2</div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">NIvel 3</div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">NIvel 4</div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">NIvel 5</div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br>
+    <!--hr style="border: 1px dashed black; top: 600px;" /-->
+    <br><br><br><br><br><br><br><br>
+    <div class="row row-cols-2">
+        <div class="col">
+            <div id="razon1"><span>Colorantes Importados SA de CV</span></div>
+            <div id="front"><img  src="img\TarjetaFront.png" /></div>
+            <div id="nombres"><span>Luis Manuel</span></div>
+            <div id="apellidos"><span>Rivera Garcia</span></div>
+            <div id="foto"> <img src="img\Foto.png" /> </div>
+        </div>
+        <div class="col">
+            <div id="capa1"></div>
+            <div id="back"><img src="img\TarjetaBackup.png" /></div>
+            <div id="capa3">NIvel 6</div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
